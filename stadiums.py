7249ac804773431391b4fe8a0223136f992ba313
Pradeep-joon <pradeepjoon0001@gmail.com>
2025-02-20 22:32:59 +0530

    Refactored code, added stadiums.py and teams.py

---
+++ stadiums.py
@@ -0,0 +1,12 @@
+ICC_STADIUMS = {
+    "India": ["Eden Gardens", "Wankhede Stadium", "Narendra Modi Stadium", "MA Chidambaram Stadium", "Chinnaswamy Stadium"],
+    "England": ["Lords", "Oval", "Edgbaston", "Headingley", "Old Trafford"],
+    "Pakistan": ["Gaddafi Stadium", "National Stadium", "Rawalpindi Cricket Stadium", "Multan Cricket Stadium", "Arbab Niaz Stadium"],
+    "Australia": ["Melbourne Cricket Ground", "Sydney Cricket Ground", "Adelaide Oval", "Gabba", "Perth Stadium"],
+    "South Africa": ["Newlands", "Wanderers Stadium", "SuperSport Park", "Kingsmead", "St George’s Park"],
+    "New Zealand": ["Eden Park", "Hagley Oval", "Basin Reserve", "University Oval", "Seddon Park"],
+    "West Indies": ["Kensington Oval", "Queen’s Park Oval", "Daren Sammy Stadium", "Providence Stadium", "Sabina Park"],
+    "Sri Lanka": ["R. Premadasa Stadium", "Pallekele International Cricket Stadium", "Galle International Stadium", "SSC Ground", "Dambulla International Stadium"],
+    "Bangladesh": ["Sher-e-Bangla National Stadium", "Zahur Ahmed Chowdhury Stadium", "Khan Shaheb Osman Ali Stadium", "Sylhet International Cricket Stadium", "Sheikh Abu Naser Stadium"],
+    "UAE": ["Dubai International Cricket Stadium", "Sharjah Cricket Stadium", "Abu Dhabi Cricket Stadium", "Ajman Oval", "ICC Academy Ground"]
+}
---
+++ teams.py
@@ -0,0 +1,74 @@
+ICC_TEAMS = {
+    "India": {
+        "batsmen": ["Rohit Sharma", "Virat Kohli", "Shubman Gill", "Suryakumar Yadav"],
+        "allrounders": ["Hardik Pandya", "Ravindra Jadeja", "Axar Patel"],
+        "fast_bowlers": ["Jasprit Bumrah", "Mohammed Shami", "Mohammed Siraj"],
+        "spinners": ["Kuldeep Yadav", "Ravi Bishnoi"]
+    },
+    "Australia": {
+        "batsmen": ["David Warner", "Steve Smith", "Marnus Labuschagne", "Travis Head"],
+        "allrounders": ["Glenn Maxwell", "Mitchell Marsh", "Marcus Stoinis"],
+        "fast_bowlers": ["Pat Cummins", "Josh Hazlewood", "Mitchell Starc"],
+        "spinners": ["Adam Zampa", "Nathan Lyon"]
+    },
+    "England": {
+        "batsmen": ["Joe Root", "Jonny Bairstow", "Jos Buttler", "Dawid Malan"],
+        "allrounders": ["Ben Stokes", "Moeen Ali", "Liam Livingstone"],
+        "fast_bowlers": ["Jofra Archer", "Mark Wood", "Chris Woakes"],
+        "spinners": ["Adil Rashid", "Rehan Ahmed"]
+    },
+    "Pakistan": {
+        "batsmen": ["Babar Azam", "Fakhar Zaman", "Imam-ul-Haq", "Abdullah Shafique"],
+        "allrounders": ["Shadab Khan", "Mohammad Nawaz", "Agha Salman"],
+        "fast_bowlers": ["Shaheen Afridi", "Haris Rauf", "Hasan Ali"],
+        "spinners": ["Abrar Ahmed", "Usama Mir"]
+    },
+    "New Zealand": {
+        "batsmen": ["Kane Williamson", "Devon Conway", "Daryl Mitchell", "Glenn Phillips"],
+        "allrounders": ["Michael Bracewell", "Mitchell Santner", "Rachin Ravindra"],
+        "fast_bowlers": ["Trent Boult", "Tim Southee", "Lockie Ferguson"],
+        "spinners": ["Ish Sodhi", "Ajaz Patel"]
+    },
+    "South Africa": {
+        "batsmen": ["Quinton de Kock", "Temba Bavuma", "Aiden Markram", "Rassie van der Dussen"],
+        "allrounders": ["Marco Jansen", "Wayne Parnell", "Andile Phehlukwayo"],
+        "fast_bowlers": ["Kagiso Rabada", "Anrich Nortje", "Lungi Ngidi"],
+        "spinners": ["Tabraiz Shamsi", "Keshav Maharaj"]
+    },
+    "Sri Lanka": {
+        "batsmen": ["Kusal Mendis", "Dimuth Karunaratne", "Pathum Nissanka", "Charith Asalanka"],
+        "allrounders": ["Dhananjaya de Silva", "Dasun Shanaka", "Wanindu Hasaranga"],
+        "fast_bowlers": ["Dushmantha Chameera", "Lahiru Kumara", "Kasun Rajitha"],
+        "spinners": ["Maheesh Theekshana", "Jeffrey Vandersay"]
+    },
+    "West Indies": {
+        "batsmen": ["Shai Hope", "Nicholas Pooran", "Shimron Hetmyer", "Brandon King"],
+        "allrounders": ["Jason Holder", "Kyle Mayers", "Roston Chase"],
+        "fast_bowlers": ["Alzarri Joseph", "Jayden Seales", "Oshane Thomas"],
+        "spinners": ["Gudakesh Motie", "Akeal Hosein"]
+    },
+    "Bangladesh": {
+        "batsmen": ["Tamim Iqbal", "Liton Das", "Najmul Hossain Shanto", "Mushfiqur Rahim"],
+        "allrounders": ["Shakib Al Hasan", "Mahmudullah", "Mehidy Hasan Miraz"],
+        "fast_bowlers": ["Taskin Ahmed", "Mustafizur Rahman", "Ebadot Hossain"],
+        "spinners": ["Nasum Ahmed", "Taijul Islam"]
+    },
+    "Afghanistan": {
+        "batsmen": ["Rahmanullah Gurbaz", "Ibrahim Zadran", "Hashmatullah Shahidi", "Najibullah Zadran"],
+        "allrounders": ["Mohammad Nabi", "Azmatullah Omarzai", "Gulbadin Naib"],
+        "fast_bowlers": ["Fazalhaq Farooqi", "Naveen-ul-Haq", "Abdul Rahman"],
+        "spinners": ["Rashid Khan", "Mujeeb Ur Rahman"]
+    },
+    "Ireland": {
+        "batsmen": ["Paul Stirling", "Andrew Balbirnie", "Harry Tector", "Lorcan Tucker"],
+        "allrounders": ["Curtis Campher", "Gareth Delany", "Mark Adair"],
+        "fast_bowlers": ["Josh Little", "Barry McCarthy", "Craig Young"],
+        "spinners": ["George Dockrell", "Ben White"]
+    },
+    "Zimbabwe": {
+        "batsmen": ["Craig Ervine", "Sean Williams", "Sikandar Raza", "Ryan Burl"],
+        "allrounders": ["Wesley Madhevere", "Tony Munyonga", "Donald Tiripano"],
+        "fast_bowlers": ["Blessing Muzarabani", "Tendai Chatara", "Richard Ngarava"],
+        "spinners": ["Wellington Masakadza", "Brandon Mavuta"]
+    }
+}
